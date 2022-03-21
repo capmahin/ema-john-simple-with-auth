04d4d8708dc3ef417d31d30870e1a1906964a532
capmahin <mahinulhuque1996@gmail.com>
2022-03-22 04:48:30 +0600

    add header and color and background

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const Header = () => {
     return (
-       <nav>
+       <nav className='header'>
        <img src={logo} alt="" />
 
        </nav>
