f0bca5c5de4f3e6eefb09fa3ded7cb2b1ff34ee9
capmahin <mahinulhuque1996@gmail.com>
2022-03-31 05:29:50 +0600

    add reviewItem

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,14 +2,18 @@ import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 import Cart from "../Cart/Cart";
-import "./Orders.css";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
   return (
     <div className="shop-container">
-      <div className="products-container"></div>
+      <div className="products-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
+      </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
       </div>
