a0989145c6c98f4fade958a3245d27b34a7d7aec
capmahin <mahinulhuque1996@gmail.com>
2022-03-31 03:01:34 +0600

    install route and add browser route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Header/Shop/Shop';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Header/Shop/Shop";
+import { Route, Router } from "react-router-dom";
 
 function App() {
   return (
-    <div >
+    <div>
       <Header></Header>
+      <Router>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        <Route path="/home" element={<Shop></Shop>}></Route>
+      </Router>
       <Shop></Shop>
-      
     </div>
   );
 }
